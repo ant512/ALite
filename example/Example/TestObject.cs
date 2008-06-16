007b4fdac14fc507e10c1b6b39d994543b3bad26
ant512 <devnull@localhost>
2008-06-16 13:39:19 +0000

    - Updated test program.

--- example/Example/TestObject.cs
+++ example/Example/TestObject.cs
@@ -45,84 +45,84 @@ namespace Example
 
 		public override DBErrorCode Fetch()
 		{
-			DataAccess data = new DataAccess();
 			DBErrorCode status = DBErrorCode.Ok;
-
-			// Prepare data access object
-			data.Procedure = "uspTestObjectSelect";
-			data.AddParameter("@ObjectID", mId);
-
-			// Attempt to retrieve data
-			if (data.Fetch())
+			
+			using (DataAccess data = new DataAccess())
 			{
-				mName = data.GetString("ObjectName");
+				// Prepare data access object
+				data.Procedure = "uspTestObjectSelect";
+				data.AddParameter("@ObjectID", mId);
 
-				// Call base method
-				base.Fetch();
-			}
-			else
-			{
-				// Fetch failed; update status
-				status = DBErrorCode.Failed;
-			}
+				// Attempt to retrieve data
+				if (data.Fetch())
+				{
+					mName = data.GetString("ObjectName");
 
-			data.Dispose();
+					// Call base method
+					base.Fetch();
+				}
+				else
+				{
+					// Fetch failed; update status
+					status = DBErrorCode.Failed;
+				}
+			}
 
 			return status;
 		}
 
 		protected override DBErrorCode Create()
 		{
-			DataAccess data = new DataAccess();
 			DBErrorCode status = DBErrorCode.Ok;
 
-			// Save to database
-			data.Procedure = "uspTestObjectCreate";
-			data.AddParameter("@ObjectID", mId);
-			data.AddParameter("@ObjectName", mName);
-			data.Save();
+			using (DataAccess data = new DataAccess())
+			{
+				// Save to database
+				data.Procedure = "uspTestObjectCreate";
+				data.AddParameter("@ObjectID", mId);
+				data.AddParameter("@ObjectName", mName);
+				data.Save();
 
-			// Call base method
-			base.Create();
-
-			data.Dispose();
+				// Call base method
+				base.Create();
+			}
 
 			return status;
 		}
 
 		protected override DBErrorCode Update()
 		{
-			DataAccess data = new DataAccess();
 			DBErrorCode status = DBErrorCode.Ok;
 
-			// Update database
-			data.Procedure = "uspTestObjectUpdate";
-			data.AddParameter("@ObjectID", mId);
-			data.AddParameter("@ObjectName", mName);
-			data.Save();
+			using (DataAccess data = new DataAccess())
+			{
+				// Update database
+				data.Procedure = "uspTestObjectUpdate";
+				data.AddParameter("@ObjectID", mId);
+				data.AddParameter("@ObjectName", mName);
+				data.Save();
 
-			// Call base method
-			base.Update();
-
-			data.Dispose();
+				// Call base method
+				base.Update();
+			}
 
 			return status;
 		}
 
 		protected override DBErrorCode Delete()
 		{
-			DataAccess data = new DataAccess();
 			DBErrorCode status = DBErrorCode.Ok;
 
-			// Delete from database
-			data.Procedure = "uspTestObjectDelete";
-			data.AddParameter("@ObjectID", mId);
-			data.Save();
+			using (DataAccess data = new DataAccess())
+			{
+				// Delete from database
+				data.Procedure = "uspTestObjectDelete";
+				data.AddParameter("@ObjectID", mId);
+				data.Save();
 
-			// Call base method
-			base.Delete();
-
-			data.Dispose();
+				// Call base method
+				base.Delete();
+			}
 
 			return status;
 		}
