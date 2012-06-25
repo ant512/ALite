ca041304b8fccfb69133cb1be6817365114c7377
ant512 <devnull@localhost>
2012-06-25 15:12:45 -0600

    Added tests.

--- source/ALite.ObjectValidator.Tests/ObjectValidatorTest.cs
+++ source/ALite.ObjectValidator.Tests/ObjectValidatorTest.cs
@@ -30,6 +30,42 @@ namespace ALite.Tests
 		}
 
 		[TestMethod]
+		public void TestStringLengthValidation()
+		{
+			var obj = new Validator();
+			obj.AddRule("name", new StringLengthValidationRule(3, 10));
+
+			var errors = new List<string>();
+
+			Assert.IsFalse(obj.Validate<string>("name", errors, "Jo"), "String is too short.");
+			Assert.IsFalse(obj.Validate<string>("name", errors, "This name is too long"), "String is too long.");
+			Assert.IsTrue(obj.Validate<string>("name", errors, "Bob"), "String should be valid.");
+		}
+
+		[TestMethod]
+		public void TestIntegerBoundsValidation()
+		{
+			var obj = new Validator();
+			obj.AddRule("value", new IntegerBoundsValidationRule(3, 8));
+
+			var errors = new List<string>();
+
+			Assert.IsFalse(obj.Validate<int>("value", errors, 2), "Integer is too small.");
+			Assert.IsFalse(obj.Validate<int>("value", errors, 9), "Integer is too large.");
+			Assert.IsTrue(obj.Validate<int>("value", errors, 5), "Integer should be valid.");
+		}
+
+		[TestMethod]
+		public void TestMissingValidator()
+		{
+			var obj = new Validator();
+
+			var errors = new List<string>();
+
+			Assert.IsTrue(obj.Validate<int>("value", errors, 2), "All values should be allowed if no rules apply.");
+		}
+
+		[TestMethod]
 		public void TestDelegateValidation()
 		{
 			var obj = new Validator();
@@ -45,9 +81,8 @@ namespace ALite.Tests
 
 			var errorMessages = new List<string>();
 
-			var pass = obj.Validate<string>("name", errorMessages, "Bob");
-
-			Assert.IsFalse(pass, "Name cannot be 'Bob'");
+			Assert.IsFalse(obj.Validate<string>("name", errorMessages, "Bob"), "Name cannot be 'Bob'");
+			Assert.IsTrue(obj.Validate<string>("name", errorMessages, "Joe"), "Name can be Joe.");
 		}
 	}
 }
